7867c647790cf9576c05ee2142a21157e2f8b85c
ptlthg <edmier12@gmail.com>
2021-10-26 12:05:13 -0400

    Make it compatible with firefox

--- sketch.js
+++ sketch.js
@@ -548,3 +548,9 @@ class Cell {
         }
     }
 }
+
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
